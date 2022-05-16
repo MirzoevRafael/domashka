be98c2786d11bf1a09bc648004bbd4f5ac83e1e1
MirzoevRafael <91758712+MirzoevRafael@users.noreply.github.com>
2022-05-17 00:11:15 +0500

    Delete dzSem-2/Односвязные списки на сайте directory

--- dzSem-2/Односвязные списки на сайте/task_three/task_three/cgi.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-bool isGet();
-void getFormData(char*& data);
-size_t getContentLength();
-void getParamValue(char*& value, const char* paramName, const char* data);
-void strDecode(char*& decStr, const char* encStr);
